b63c1d544881fd9516953f76ca8cf93516116679
Vy0x2 <denispaul43@gmail.com>
2023-02-18 19:52:36 +0100

    Revert "Fix division by zero when joystick deadzone is at maximum"
    This reverts commit dcbb0b3b87e778ab919b0a199247d2e1de5e2b82.

--- src/engine/client/input.cpp
+++ src/engine/client/input.cpp
@@ -148,7 +148,7 @@ void CInput::ConchainJoystickGuidChanged(IConsole::IResult *pResult, void *pUser
 
 float CInput::GetJoystickDeadzone()
 {
-	return minimum(g_Config.m_InpControllerTolerance / 50.0f, 0.995f);
+	return g_Config.m_InpControllerTolerance / 50.0f;
 }
 
 CInput::CJoystick::CJoystick(CInput *pInput, int Index, SDL_Joystick *pDelegate)
