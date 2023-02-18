7287bc65c14de4fe6f27340f6670041c63ca1922
Vy0x2 <denispaul43@gmail.com>
2023-02-18 19:52:36 +0100

    Revert "removed bitmask.h"
    This reverts commit cdfbfbdd3e300d678d915138da4fe035aec2c08f.

---
+++ src/base/tl/bitmask.h
@@ -0,0 +1,8 @@
+/* (c) DDNet Team. See licence.txt in the root of the distribution for more information.     */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+#ifndef BASE_TL_BITMASK_H
+#define BASE_TL_BITMASK_H
+
+#include <bitset>
+
+#endif
