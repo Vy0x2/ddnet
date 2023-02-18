4dd8ded011848e2cadd9e0ae1854eae3ff6745b3
Vy0x2 <denispaul43@gmail.com>
2023-02-18 19:54:09 +0100

    Revert "Use `bool` instead of `int` for `m_InputGrabbed`"
    This reverts commit 86531472c247fda48cc56d6927d0ec64d39fd91b.

--- src/engine/client/input.cpp
+++ src/engine/client/input.cpp
@@ -47,7 +47,7 @@ CInput::CInput()
 	mem_zero(m_aInputState, sizeof(m_aInputState));
 
 	m_InputCounter = 1;
-	m_InputGrabbed = false;
+	m_InputGrabbed = 0;
 
 	m_MouseDoubleClick = false;
 
@@ -272,14 +272,14 @@ bool CInput::MouseRelative(float *pX, float *pY)
 
 void CInput::MouseModeAbsolute()
 {
-	m_InputGrabbed = false;
+	m_InputGrabbed = 0;
 	SDL_SetRelativeMouseMode(SDL_FALSE);
 	Graphics()->SetWindowGrab(false);
 }
 
 void CInput::MouseModeRelative()
 {
-	m_InputGrabbed = true;
+	m_InputGrabbed = 1;
 #if !defined(CONF_PLATFORM_ANDROID) // No relative mouse on Android
 	SDL_SetRelativeMouseMode(SDL_TRUE);
 #endif
